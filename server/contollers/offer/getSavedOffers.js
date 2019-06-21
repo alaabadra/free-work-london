43e85ab0b7df89486dd70f89246637acb2a9c05a
alaabadra <alaabadra4@gmail.com>
2019-06-22 00:59:21 +0300

    get result in file getsaveroffers.js from queries getsavedoffers in offer folder in queries

--- server/contollers/offer/getSavedOffers.js
+++ server/contollers/offer/getSavedOffers.js
@@ -1,4 +1,18 @@
-//get page saved offer
-module.exports = (req, res) => {
-    res.send(' get my saved offer');
-  };
+
+const { getSavedOffer } = require('../../database/queries/offer/getSavedOffers');
+
+module.exports = (req, res, next) => {
+  const { memberId } = req.params;
+  getSavedOffer(memberId)
+    .then(result => 
+      {
+        console.log(result);
+        
+      res.send({
+      error: null,
+      data: result.rows,
+    })
+    
+  })
+    .catch(() => next({ code: 500, msg: 'internal server error' }));
+};

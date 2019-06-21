1516029b3e486c01f962c44032908ff3d9f1970a
alaabadra <alaabadra4@gmail.com>
2019-06-21 12:46:33 +0300

    add this route             <Route exact path=/app/offers/my-app component={MyApplications} />

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import {
   Header,
   Footer,
   PageNotFound,
+  MyApplications,
 } from './Components';
 
 
@@ -29,6 +30,8 @@ export default class App extends Component{
             <Route exact path="/login" component={Login} />
             <Route exact path="/signup" component={Signup} />
             <Route exact path="/home" component={Home} />
+            <Route exact path="/app/offers/my-app" component={MyApplications} />
+
             <Route component={PageNotFound} />
           </Switch>
           <Footer />

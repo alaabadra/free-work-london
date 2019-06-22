341e2de20ca30ce1530da3d33b70046bce8aceb9
alaabadra <alaabadra4@gmail.com>
2019-06-22 18:10:36 +0300

    index.jsx in offerDetails: fetch (offers,offer-apps,myapp)from response from db in which this response put it in state for using it in render

--- client/src/Components/OfferDetails/ApplicationCard/index.jsx
+++ client/src/Components/OfferDetails/ApplicationCard/index.jsx
@@ -5,16 +5,112 @@ import './style.css';
 export default class OfferDetails extends Component{
     
     state={
-        
+        application:'',
+        userInfo:'',
+        showWrongAlert:''
     }
     handleProfile=()=>{
-
+        const {application:{username},history} = this.props
+        history.push(`/app/profile/${username}`)
     }
     handleHireMe=()=>{
-
+        const {application:{member_id}, match}= this.props;
+        const {params:{offerId}}= match;
+        fetch(`/api/v1/hired-member`,{
+            method:'POST',
+            credentials:'same-origin',
+            body:JSON.stringify({
+                member_id,
+                offer_id:offerId,
+            })
+        })
+        .then(res=>{
+            if(res.data){
+                console.log(res.data);
+                
+            }
+        })
+        .catch(() =>
+        this.setState(
+          {
+            showWrongAlert: true,
+          },
+          () =>
+            setTimeout(() => {
+              this.setState({ showWrongAlert: false });
+            }, 5000)
+        )
+      );
+    }
+    handleAccept=()=>{
+        const {userInfo} = this.state;
+        const {match}= this.props;
+        const {params:{offerId}}= match;
+        fetch(`/api/v1/hired-member/${userInfo.id}`,{
+            method:'PATCH',
+            body:JSON.stringify({offer_id:offerId,status:'accepted'}),
+            headers:{
+                'Content-Type':'application/json'
+            }
+        })
+        .then(response=>response.json())
+        .then(res=>{
+            if(res.data){
+                console.log(res.data)
+            }
+        })
+        .catch(() =>
+        this.setState(
+          {
+            showWrongAlert: true,
+          },
+          () =>
+            setTimeout(() => {
+              this.setState({ showWrongAlert: false });
+            }, 5000)
+        )
+      );
+    }
+    handleRefuse=()=>{
+        const {userInfo} = this.state;
+        const {match}= this.props;
+        const {params:{offerId}}= match;
+        fetch(`/api/v1/hired-member/${userInfo.id}`,{
+            method:'PATCH',
+            body:JSON.stringify({offer_id:offerId,status:'refused'}),
+            headers:{
+                'Content-Type':'application/json'
+            }
+        })
+        .then(response=>response.json())
+        .then(res=>{
+            if(res.data){
+                console.log(res.data)
+            }
+        })
+        .catch(() =>
+        this.setState(
+          {
+            showWrongAlert: true,
+          },
+          () =>
+            setTimeout(() => {
+              this.setState({ showWrongAlert: false });
+            }, 5000)
+        )
+      );
     }
     componentDidMount(){
-        console.log('propsApp',this.props);
+        const {application} = this.props;
+        const userInfo = {
+            id: 2,
+            fullName: 'Alaa Badra',
+            username: 'alaabadra',
+            avatar:
+              'https://m.media-amazon.com/images/M/MV5BMTcxOTk4NzkwOV5BMl5BanBnXkFtZTcwMDE3MTUzNA@@._V1_.jpg',
+          };
+          this.setState({application,userInfo})
+        {console.log(this.props)}
 
     }
     render(){
@@ -26,6 +122,7 @@ export default class OfferDetails extends Component{
            
           } = this.props.application;
           const { viewProfile, HireMe} = this.props;
+          const {userInfo} = this.state;
         return(
             <>
             <Container className="application-card__container">
@@ -49,10 +146,29 @@ export default class OfferDetails extends Component{
                                     Hire me
                                     
                                 </Button>
-                            ):(
-                                <Card.Text className={`application-card__${status}`}>
-                                    {status}
-                                </Card.Text>
+                            ):(   <Card.Text>
+                              {userInfo.id === this.props.application.member_id &&
+                            this.props.application.status === 'pending' ? (
+                                <>
+                                  <Button
+                                    className="application-card__button refuse-btn"
+                                    onClick={this.handleRefuse}
+                                  >
+                                    Refuse
+                                  </Button>
+                                  <Button
+                                    className="application-card__button accept-btn"
+                                    onClick={this.handleAccept}
+                                  >
+                                    Accept
+                                  </Button>
+                                </>
+                              ) : (
+                                <span >
+                                  {status}
+                                </span>
+                              )}
+                            </Card.Text>
                             )}
                         </div>
                     </Card.Body>

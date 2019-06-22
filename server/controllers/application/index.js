514e7b95eaf83809a5cd39f35e39908dc83c7e0a
alaabadra <alaabadra4@gmail.com>
2019-06-22 20:42:01 +0300

    getofferapps in which i use query for exract result from db and using this result in file getofferapps in controller to show in browser

--- server/controllers/application/index.js
+++ server/controllers/application/index.js
@@ -1,7 +1,7 @@
 const addHireMember = require('./addHiredMember');
 const updateHireMember = require('./patchHiredMember');
 const addApplication = require('./addApplication');
-// const getOfferApplication = require('./getOfferApplication');
+const getOfferApplications = require('./getOfferApplications');
 const getMyApplications = require('./getMyApplications');
 const getMyApplication = require('./getMyApplication');
 
@@ -9,7 +9,7 @@ module.exports = {
   addHireMember,
   updateHireMember,
   addApplication,
-  // getOfferApplication,
+  getOfferApplications,
   getMyApplication,
   getMyApplications,
 };
